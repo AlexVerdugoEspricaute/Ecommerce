25bfa4b5422ca019389d9d8e3eac928b88010df4
AlexVerdugo <alex.verdugo.-@hotmail.com>
2024-02-28 21:50:15 -0300

    Creacion de Capas y logica

--- SistemaVenta.IOC/Dependencia.cs
+++ SistemaVenta.IOC/Dependencia.cs
@@ -7,6 +7,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.DAL.Repositorios;
+using SistemaVenta.Utility;
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.BLL.Servicios;
 
 namespace SistemaVenta.IOC
 {
@@ -17,6 +22,20 @@ namespace SistemaVenta.IOC
             services.AddDbContext<DbventasContext>(options => {
                 options.UseSqlServer(configuration.GetConnectionString("cadenaSQL"));
             });
+
+            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IVentaRepository, VentaRepository>();
+
+            services.AddAutoMapper(typeof(AutoMapperProfile));
+
+            services.AddScoped<IRolService, RolService>();
+            services.AddScoped<IUsuarioService,UsuarioService>();
+            services.AddScoped<ICategoriaService, CategoriaService>();
+            services.AddScoped<IProductoService, ProductoService>();
+            services.AddScoped<IVentaService, VentaService>();
+            services.AddScoped<IDashBoardService, DashBoardService>();
+            services.AddScoped<IMenuService, MenuService>();
+
         }
     }
 }
